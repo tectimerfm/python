b35de90b8e5d6bcab048f7a59067b6693916f650
tectimerfm <137183150+tectimerfm@users.noreply.github.com>
2026-06-12 10:26:08 +0100

    Add test_snyk_token.py for secret scanning test

---
+++ test_snyk_token.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+snyk_token = "fake_snyk_token_for_secret_scanning_test_123456"
+
+def main():
+    print("Token de teste carregado")
+
+if __name__ == "__main__":
+    main()
